c35d890d528e48a1d15dd92d1d0a2a6adcc3df40
Riyan.rz <95316648+Stinkyrz@users.noreply.github.com>
2021-12-01 21:30:28 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,7 +84,7 @@ class Bot(Client):
 
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/SharingUserbot"
+            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/Stinkyrz"
         )
         self.username = usr_bot_me.username
 
