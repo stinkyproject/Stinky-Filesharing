56a755622f229df377a911fcfdfc05c95d0bbccf
Riyan.rz <95316648+Stinkyrz@users.noreply.github.com>
2021-12-23 10:31:40 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/SharingUserbot untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/Stinkysupport untuk Bantuan"
                 )
                 sys.exit()
         if FORCE_SUB_GROUP:
@@ -64,7 +64,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_GROUP dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Group Saat Ini: {FORCE_SUB_GROUP}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/StinkyGroupChatt untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/Stinkysupport untuk Bantuan"
                 )
                 sys.exit()
         try:
@@ -84,7 +84,7 @@ class Bot(Client):
 
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/Stinkyrz"
+            f"[🔥 BOT FILE SHARING BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/Stinkyrz"
         )
         self.username = usr_bot_me.username
 
